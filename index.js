6199c3c2fea4df72467f8c11ba69cfb424edaa69
Rosshoven <Michaeledwardross@gmail.com>
2023-01-18 11:15:40 -0600

    Adding to list and styling css

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 function App(){
+
+  // initial state
   const [todos, setTodos] = React.useState([
     {
-      text: 'learn react',
+      text: 'Learn react',
       isCompleted: false,
     },
     {
-      text: 'meet friend for lunch',
+      text: 'Meet Mike for lunch',
       isCompleted: false,
     },
     {
-      text: 'build todo app',
+      text: 'Build todo app',
       isCompleted: false,
-    }        
+    }, 
+    {
+      text: 'Use state for personal gain',
+      isCompleted: false,
+    }          
   ])
 
+  // write jsx so the above displays in the browser, writitng a loop to go through the list
   return(
     <>
-      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+      {todos.map((todo, i) => <div className="todo" key={i}>{todo.text}</div>)}
     </>
   );
 }
 
 ReactDOM.render(
-  <App/>,
+  <App  />,
   document.getElementById('root')
 );
