5bb2d2c3b241ea1b4ad762e9d6e304a05a88a82a
Rosshoven <Michaeledwardross@gmail.com>
2023-01-18 13:33:35 -0600

    separated TodoForm into own component

---
+++ form.js
@@ -0,0 +1,30 @@
+function TodoForm({addTodo}) {
+      // Adding new variable which is a managed variable
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = e => {
+    // prevent reloading of the page
+    e.preventDefault();
+    // check for existing value
+    if (!value) return
+    // construct new list using existing list plus new todo
+    addTodo(value);
+    // clear out the form
+    setValue('');
+  }
+    
+  return (
+    /* creating form  that has an input element for user to enter value. When form is submitted we handle event with handleSubmit*/
+    <form onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            className="input" 
+            value={value}
+            placeholder="Add todo ..."
+            onChange={e => setValue(e.target.value)}
+            />
+    </form>
+    );
+}
+
+// export default <TodoForm />;
--- index.js
+++ index.js
@@ -1,42 +1,36 @@
+// import TodoForm from "form.";
+
 function App(){
 
   // initial state
   const [todos, setTodos] = React.useState([
-    {
-      text: 'Learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'Meet Mike for lunch',
-      isCompleted: false,
-    },
-    {
-      text: 'Build todo app',
-      isCompleted: false,
-    }, 
-    {
-      text: 'Use state for personal gain',
-      isCompleted: false,
-    }          
+    // {
+    //   text: 'Learn react',
+    //   isCompleted: false,
+    // },
+    // {
+    //   text: 'Meet Mike for lunch',
+    //   isCompleted: false,
+    // },
+    // {
+    //   text: 'Build todo app',
+    //   isCompleted: false,
+    // }, 
+    // {
+    //   text: 'Use state for personal gain',
+    //   isCompleted: false,
+    // }, 
   ]);
 
-  // Adding new variable which is a managed variable
-  const [value, setValue] = React.useState('');
 
-  const handleSubmit = e => {
-    // prevent reloading of the page
-    e.preventDefault();
-    // check for existing value
-    if (!value) return
-    // construct new list using existing list plus new todo
-    const newTodos = [...todos, {text:value, isCompleted:false}];
-    // going from current state to new state
-    setTodos(newTodos);
-    // clear out the form
-    setValue('');
-  }
+const addTodo = text => {
+  const newTodos = [...todos, {text:text, isCompleted:false}];
+  // going from current state to new state
+  setTodos(newTodos);
+}
 
-  const removeTodo = e => {
+
+const removeTodo = e => {
     const index = Number(e.target.id);
     // temporary list as current list of todos
     let temp = [...todos];
@@ -48,16 +42,7 @@ function App(){
   // write jsx so the above displays in the browser, writitng a loop to go through the list
   return(<>
       {todos.map((todo, i) => <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
-      {/* creating form  that has an input element for user to enter value. When form is submitted we handle event with handleSubmit*/}
-      <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        className="input" 
-        value={value}
-        placeholder="Add todo ..."
-        onChange={e => setValue(e.target.value)}
-        />
-      </form>
+      <TodoForm addTodo={addTodo} />
     </>);
 }
 
